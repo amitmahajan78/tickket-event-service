bb1b63cb37f470d5feed807cf15efa16b651de46
amitmahajan <amitmahajan@post.com>
2020-12-06 08:36:24 +0000

    base project

--- cmd-event-service/src/main/java/com/thesaastech/tickket/service/event/web/rest/EventCommandResources.java
+++ cmd-event-service/src/main/java/com/thesaastech/tickket/service/event/web/rest/EventCommandResources.java
@@ -1,16 +1,19 @@
 package com.thesaastech.tickket.service.event.web.rest;
 
 import com.thesaastech.tickket.data.commands.CreateEvent;
-import com.thesaastech.tickket.service.event.web.rest.dto.CreateEventRequestData;
+import com.thesaastech.tickket.data.query.FindDuplicateEvents;
 import lombok.extern.slf4j.Slf4j;
 import org.axonframework.extensions.reactor.commandhandling.gateway.ReactorCommandGateway;
+import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 @Slf4j
@@ -27,24 +30,28 @@ public class EventCommandResources {
         this.queryGateway = queryGateway;
     }
 
+
     @PostMapping(path = "/event")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Mono<UUID> creatEvent(@Valid @RequestBody CreateEventRequestData createEventRequestData) throws ExecutionException, InterruptedException {
+    public ResponseEntity<Mono<String>> creatEvent(@Valid @RequestBody CreateEventRequestData createEventRequestData) throws ExecutionException, InterruptedException {
+        log.info("adasdadahjhhhajsdahdgaskdgsakdsadgsajkgdaskjdgaksdgsadgskadgsadjkgkdjasgdksajgdkj");
+        //check duplicate
+        CompletableFuture<Boolean> eventResponseData = queryGateway.query(new FindDuplicateEvents(createEventRequestData.getTitle(), createEventRequestData.getType(),
+                createEventRequestData.getDateTime(),
+                createEventRequestData.getVenueCity()), ResponseTypes.instanceOf(Boolean.class));
+
         UUID eventId = UUID.randomUUID();
 
-        log.info(createEventRequestData.toString());
+        if (eventResponseData.get()) {
+            return ResponseEntity.status(HttpStatus.ACCEPTED)
+                    .body(Mono.when(reactorCommandGateway.send(new CreateEvent(eventId, "tomdoo",
+                            createEventRequestData.getType(), createEventRequestData.getTitle(), createEventRequestData.getDescription(),
+                            createEventRequestData.getDateTime(), createEventRequestData.getVenueAddressLine(), createEventRequestData.getVenueCity(),
+                            createEventRequestData.getVenuePostcode())))
+                            .then(Mono.just(eventId.toString())));
+        }
 
-       /* CompletableFuture<List<Events>> eventList = queryGateway.query(new FindDuplicateEventQuery(createEventRequestData.getTitle(), createEventRequestData.getType(),
-                createEventRequestData.getDateTime(), createEventRequestData.getVenueAddressLine(), createEventRequestData.getVenueCity(),
-                createEventRequestData.getVenuePostcode()), ResponseTypes.multipleInstancesOf(Events.class));
-
-        log.info(eventList.get().toString());*/
-
-        return Mono.when(reactorCommandGateway.send(new CreateEvent(eventId, "tomdoo",
-                createEventRequestData.getType(), createEventRequestData.getTitle(), createEventRequestData.getDescription(),
-                createEventRequestData.getDateTime(), createEventRequestData.getVenueAddressLine(), createEventRequestData.getVenueCity(),
-                createEventRequestData.getVenuePostcode())))
-                .then(Mono.just(eventId));
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(Mono.just("Duplicate events found. Try creating events with different details."));
     }
 
 }
